8524ba70b33917fcf1bc8bca0fdd27f3e197f2ff
evan.gillard <evanjgillard@gmail.com>
2019-07-23 16:49:58 +1000

    added ability to export image graph of performance

--- src/Benchmark.java
+++ src/Benchmark.java
@@ -6,10 +6,15 @@ import javafx.scene.Scene;
 import javafx.scene.canvas.Canvas;
 import javafx.stage.Screen;
 import javafx.stage.Stage;
+import org.knowm.xchart.BitmapEncoder;
+import org.knowm.xchart.XYChart;
+import org.knowm.xchart.XYChartBuilder;
+import org.knowm.xchart.XYSeries;
 import pfx.SizedFXApp;
 
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.Arrays;
 import java.util.Vector;
 
 public class Benchmark extends Application {
@@ -20,9 +25,16 @@ public class Benchmark extends Application {
 
     boolean scaling = true;
 
-    static int frames = 50;
+    static int frames = 100;
+    static int repeatTimes = 10;
 
-    long[][] frameTimes = new long[10][frames]; // new long[timesToRepeat][frameAnimationLength]
+    int frameIndex = 0;
+
+    //long[] avgFrameMs;
+    double[] xAvgFrames = new double[repeatTimes];
+    double[] yAvgFrames = new double[repeatTimes];
+
+    long[][] frameTimes = new long[repeatTimes][frames]; // new long[timesToRepeat][frameAnimationLength]
 
     StringBuilder log = new StringBuilder();
 
@@ -80,9 +92,32 @@ public class Benchmark extends Application {
                 if (size == frameTimes.length){
                     for(long[] vals: frameTimes){
                         long avgFrameGap = (vals[vals.length-1] - vals[0]) / (frames * 1000000l);
-                        log.append(avgFrameGap); log.append("\n"); //
+                        log.append(avgFrameGap); log.append("\n");
+
+                        xAvgFrames[frameIndex] = frameIndex;
+                        yAvgFrames[frameIndex] = avgFrameGap;
+                        frameIndex++;
                     }
-                    try {FileWriter fw = new FileWriter("logs/"+app.name()+".log"); fw.write(log.toString()); fw.close();} catch (IOException e) {e.printStackTrace();}
+                    try {
+                        FileWriter fw = new FileWriter("logs/"+app.name()+".log" );
+                        fw.write(log.toString()); fw.close();
+                        System.out.println(Arrays.toString(yAvgFrames));
+
+                        //Chart Code
+
+                        XYChart chart = new XYChartBuilder().width(800).height(600).title("Frame Calculation Time (BB)")
+                                .xAxisTitle("size").yAxisTitle("ms").build();
+                        chart.getStyler().setYAxisMin(0d);
+                        chart.getStyler().setDefaultSeriesRenderStyle(XYSeries.XYSeriesRenderStyle.Scatter);
+                        chart.getStyler().setLegendVisible(false);
+                        chart.getStyler().setMarkerSize(8);
+                        chart.addSeries("ms per Frame", xAvgFrames, yAvgFrames);
+                        //Chart Code
+
+                        BitmapEncoder.saveBitmap(chart, "./logs/FCT_Bouncing_Balls", BitmapEncoder.BitmapFormat.PNG);
+                    } catch (IOException e) {e.printStackTrace();}
+
+
                     if (apps.size() == 0) {
                         System.exit(0);
                     }
@@ -99,6 +134,7 @@ public class Benchmark extends Application {
         }.start();
 
         stage.show();
+
     }
 
 
