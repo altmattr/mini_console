440242f161e01d9d51591fafba33b7655074b601
jkennedy <jack.kennedy@imc.com>
2019-09-02 20:30:55 +1000

    Some updates to clarify Benchmark

--- src/Benchmark.java
+++ src/Benchmark.java
@@ -21,27 +21,21 @@ import java.util.Vector;
 
 public class Benchmark extends Application {
 
-    Canvas c;
-    Vector<SizedFXApp> apps = new Vector();
-    SizedFXApp app;
+    private static final int NUM_FRAMES_PER_RUN = 100;
+    private static final int NUM_RUNS = 10;
+    private final Vector<SizedFXApp> apps = new Vector();
+    private final StringBuilder log = new StringBuilder();
+    private Canvas canvas;
+    private SizedFXApp app;
 
-    boolean scaling = true;
-
-    static int frames = 100;
-    static int repeatTimes = 100;
-
-    int frameIndex = 0;
+    private final boolean scaling = true;
 
     //long[] avgFrameMs;
-    double[] xAvgFrames = new double[repeatTimes];
-    double[] yAvgFrames = new double[repeatTimes];
 
-    long[][] frameTimes = new long[repeatTimes][frames]; // new long[timesToRepeat][frameAnimationLength]
-
-    StringBuilder log = new StringBuilder();
 
     @Override
-    public void start(Stage stage) throws Exception{
+    public void start(Stage stage) throws
+            Exception {
 
         Rectangle2D bounds = Screen.getPrimary().getVisualBounds();
         stage.setX(bounds.getMinX());
@@ -53,64 +47,80 @@ public class Benchmark extends Application {
         Scene scene = new Scene(root);
         stage.setScene(scene);
         stage.setFullScreen(true);
-        c = new Canvas(bounds.getWidth(), bounds.getHeight());
-        root.getChildren().add(c);
+        canvas = new Canvas(bounds.getWidth(), bounds.getHeight());
+        root.getChildren().add(canvas);
 
         //apps.add(new benchmarks.BouncingBall(c.getGraphicsContext2D()));
-        apps.add(new benchmarks.Nyan(c.getGraphicsContext2D()));
+        apps.add(new benchmarks.Nyan(canvas.getGraphicsContext2D()));
         app = apps.remove(0);
-        log.append(app.name()); log.append("\n");
-        log.append(app.description()); log.append("\n");
+        log.append(app.name());
+        log.append("\n");
+        log.append(app.description());
+        log.append("\n");
 
         app.setSize(0);
-	
-	scene.addEventHandler(KeyEvent.KEY_PRESSED, (evt) -> {
-		if(evt.getCode() == KeyCode.F4) { // added kill switch for Raspberry Pi
-			Platform.exit();
-			System.exit(0);
-		}
-	});
 
-        new AnimationTimer(){
-            int repeat = 0;
-            int size = 0;
-            public void handle(long currentNanoTime){
-                frameTimes[size][repeat] = currentNanoTime;
+        scene.addEventHandler(KeyEvent.KEY_PRESSED, (evt) -> {
+            if (evt.getCode() == KeyCode.F4) { // added kill switch for Raspberry Pi
+                Platform.exit();
+                System.exit(0);
+            }
+        });
 
-                c.getGraphicsContext2D().save();
+        new AnimationTimer() {
+            private final long[][] frameTimes = new long[NUM_RUNS][NUM_FRAMES_PER_RUN];
+
+            int frameIndex = 0;
+            int runIndex = 0;
+
+            @Override
+            public void handle(long aCurrentNanoTime) {
+                frameTimes[runIndex][frameIndex] = aCurrentNanoTime;
+
+                canvas.getGraphicsContext2D().save();
                 if (scaling) {
-                    c.getGraphicsContext2D().scale(bounds.getWidth() / app.width, bounds.getHeight() / app.height);
+                    canvas.getGraphicsContext2D().scale(bounds.getWidth() / app.width, bounds.getHeight() / app.height);
                 } else {
                     double xoffset = (bounds.getWidth() - app.width) / 2;
                     double yoffset = (bounds.getHeight() - app.height) / 2;
-                    c.getGraphicsContext2D().translate(xoffset, yoffset);
+                    canvas.getGraphicsContext2D().translate(xoffset, yoffset);
                 }
 
                 app.draw();
-                c.getGraphicsContext2D().restore();
+                canvas.getGraphicsContext2D().restore();
 
-                repeat++;
+                frameIndex++;
 
-                if (repeat == frameTimes[size].length){
-                    size++;
-                    repeat = 0;
-                    app.setSize(size);
+                if (frameIndex == NUM_FRAMES_PER_RUN) {
+                    runIndex++;
+                    frameIndex = 0;
+                    app.setSize(runIndex);
                     app.settings();
                     app.setup();
                 }
-                if (size == frameTimes.length){
-                    for(long[] vals: frameTimes){
-                        long avgFrameGap = (vals[vals.length-1] - vals[0]) / (frames * 1000000l);
-                        log.append(avgFrameGap); log.append("\n");
 
-                        xAvgFrames[frameIndex] = frameIndex;
-                        yAvgFrames[frameIndex] = avgFrameGap;
-                        frameIndex++;
+                if (runIndex == NUM_RUNS) {
+                    System.out.println("Terminating");
+
+                    double[] runAvgFrameMs = new double[NUM_RUNS];
+                    double[] runSize = new double[NUM_RUNS];
+                    for (int i = 0; i < NUM_RUNS; i++) {
+                        long[] timesForRun = frameTimes[i];
+                        long totalRunTimeNs = timesForRun[timesForRun.length - 1] - timesForRun[0];
+                        double avgFrameGapMs = totalRunTimeNs / (NUM_FRAMES_PER_RUN * 1e6);
+                        log.append(avgFrameGapMs);
+                        log.append("\n");
+
+                        runSize[i] = i+1;
+                        runAvgFrameMs[i] = avgFrameGapMs;
+                        runIndex++;
                     }
+
                     try {
-                        FileWriter fw = new FileWriter("logs/"+app.name()+".log" );
-                        fw.write(log.toString()); fw.close();
-                        System.out.println(Arrays.toString(yAvgFrames));
+                        FileWriter fw = new FileWriter("logs/" + app.name() + ".log");
+                        fw.write(log.toString());
+                        fw.close();
+                        System.out.println(Arrays.toString(runAvgFrameMs));
 
                         //Chart Code
 
@@ -120,22 +130,22 @@ public class Benchmark extends Application {
                         chart.getStyler().setDefaultSeriesRenderStyle(XYSeries.XYSeriesRenderStyle.Scatter);
                         chart.getStyler().setLegendVisible(false);
                         chart.getStyler().setMarkerSize(8);
-                        chart.addSeries("ms per Frame", xAvgFrames, yAvgFrames);
+                        chart.addSeries("ms per Frame", runSize, runAvgFrameMs);
                         //Chart Code
 
                         BitmapEncoder.saveBitmap(chart, "./logs/FCT_Bouncing_Balls", BitmapEncoder.BitmapFormat.PNG);
-                    } catch (IOException e) {e.printStackTrace();}
-
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
 
                     if (apps.size() == 0) {
                         System.exit(0);
-                    }
-                    else {
+                    } else {
                         app = apps.remove(0);
                         System.out.println(app.name());
                         System.out.println(app.description());
-                        repeat = 0;
-                        size = 0;
+                        frameIndex = 0;
+                        runIndex = 0;
                         app.setSize(0);
                     }
                 }
@@ -145,7 +155,6 @@ public class Benchmark extends Application {
         stage.show();
 
     }
-
 
     public static void main(String[] args) {
         launch(args);
--- src/benchmarks/Nyan.java
+++ src/benchmarks/Nyan.java
@@ -11,32 +11,43 @@ public class Nyan extends SizedFXApp {
     PVector[] gravity;
     Image nyan;
 
-    int cats;
-
     public Nyan(GraphicsContext g) {
         super(g);
     }
-    public String name(){return "NyanCat";}
-    public String description(){return "Now the music is stuck in your head.";}
 
-    public void settings(){
-        size(1200,800);
-        cats = SIZE;
-        location = new PVector[cats];
-        velocity = new PVector[cats];
-        gravity = new PVector[cats];
+    @Override
+    public String name() {
+        return "NyanCat";
+    }
+
+    @Override
+    public String description() {
+        return "Now the music is stuck in your head.";
+    }
+
+    @Override
+    public void settings() {
+        size(1200, 800);
+        location = new PVector[SIZE];
+        velocity = new PVector[SIZE];
+        gravity = new PVector[SIZE];
 
         nyan = new Image("nyan.png");
-        for(int i = 0; i < cats; i++) {
-            location[i] = new PVector(600f + random(-600,600), 400f+random(-400,400)); // TODO: deal with these f's that should not be here
-            velocity[i] = new PVector(random(-4,4), random(0,1));
-            gravity[i] = new PVector(0f, 0.2f);
+        for (int i = 0; i < SIZE; i++) {
+            location[i] = new PVector(
+                    600 + random(-600, 600),
+                    400 + random(-400, 400));
+            velocity[i] = new PVector(
+                    random(-4, 4),
+                    random(0, 1));
+            gravity[i] = new PVector(0, 0.2f);
         }
     }
 
+    @Override
     public void draw(){
         background(0);
-        for(int i = 0; i < cats; i++) {
+        for(int i = 0; i < SIZE; i++) {
             location[i].add(velocity[i]);
             velocity[i].add(gravity[i]);
 
