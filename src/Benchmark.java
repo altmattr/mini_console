edc9cdcc2d3fee2a2cd60c18f5ce9d284690740f
jkennedy <jack.kennedy@imc.com>
2019-09-07 21:22:03 +1000

    Split Benchamrk class into functions

--- src/Benchmark.java
+++ src/Benchmark.java
@@ -1,3 +1,4 @@
+import benchmarks.CircleSize;
 import javafx.animation.AnimationTimer;
 import javafx.application.Application;
 import javafx.application.Platform;
@@ -5,9 +6,11 @@ import javafx.geometry.Rectangle2D;
 import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
 import javafx.stage.Screen;
 import javafx.stage.Stage;
-import javafx.scene.input.*;
 import org.knowm.xchart.BitmapEncoder;
 import org.knowm.xchart.XYChart;
 import org.knowm.xchart.XYChartBuilder;
@@ -20,24 +23,28 @@ import java.util.Arrays;
 import java.util.Vector;
 
 public class Benchmark extends Application {
-
-    private static final int NUM_FRAMES_PER_RUN = 100;
-    private static final int NUM_RUNS = 10;
-    private final Vector<SizedFXApp> apps = new Vector();
-    private final StringBuilder log = new StringBuilder();
-    private Canvas canvas;
-    private SizedFXApp app;
-
-    private final boolean scaling = true;
-
-    //long[] avgFrameMs;
-
-
     @Override
-    public void start(Stage stage) throws
-            Exception {
-
+    public void start(Stage stage) {
         Rectangle2D bounds = Screen.getPrimary().getVisualBounds();
+        Canvas canvas = setupStage(stage, bounds);
+
+        new AnimationTimerImpl(
+                canvas,
+                bounds,
+                apps(canvas.getGraphicsContext2D()))
+                .start();
+
+        stage.show();
+    }
+
+    private Vector<SizedFXApp> apps(GraphicsContext graphicsContext) {
+        Vector<SizedFXApp> apps = new Vector<>();
+        //apps.add(new benchmarks.BouncingBall(c.getGraphicsContext2D()));
+        apps.add(new CircleSize(graphicsContext));
+        return apps;
+    }
+
+    private Canvas setupStage(Stage stage, Rectangle2D bounds) {
         stage.setX(bounds.getMinX());
         stage.setY(bounds.getMinY());
         stage.setWidth(bounds.getWidth());
@@ -45,20 +52,6 @@ public class Benchmark extends Application {
 
         Group root = new Group();
         Scene scene = new Scene(root);
-        stage.setScene(scene);
-        stage.setFullScreen(true);
-        canvas = new Canvas(bounds.getWidth(), bounds.getHeight());
-        root.getChildren().add(canvas);
-
-        //apps.add(new benchmarks.BouncingBall(c.getGraphicsContext2D()));
-        apps.add(new benchmarks.Nyan(canvas.getGraphicsContext2D()));
-        app = apps.remove(0);
-        log.append(app.name());
-        log.append("\n");
-        log.append(app.description());
-        log.append("\n");
-
-        app.setSize(0);
 
         scene.addEventHandler(KeyEvent.KEY_PRESSED, (evt) -> {
             if (evt.getCode() == KeyCode.F4) { // added kill switch for Raspberry Pi
@@ -67,93 +60,152 @@ public class Benchmark extends Application {
             }
         });
 
-        new AnimationTimer() {
-            private final long[][] frameTimes = new long[NUM_RUNS][NUM_FRAMES_PER_RUN];
+        stage.setScene(scene);
+        stage.setFullScreen(true);
+        Canvas canvas = new Canvas(bounds.getWidth(), bounds.getHeight());
+        root.getChildren().add(canvas);
+        return canvas;
+    }
 
-            int frameIndex = 0;
-            int runIndex = 0;
+    private static class RunStats {
+        final double[] averageFrameMs;
+        final double[] size;
 
-            @Override
-            public void handle(long aCurrentNanoTime) {
-                frameTimes[runIndex][frameIndex] = aCurrentNanoTime;
+        private RunStats(double[] averageFrameMs, double[] size) {
+            this.averageFrameMs = averageFrameMs;
+            this.size = size;
+        }
+    }
 
-                canvas.getGraphicsContext2D().save();
-                if (scaling) {
-                    canvas.getGraphicsContext2D().scale(bounds.getWidth() / app.width, bounds.getHeight() / app.height);
+    private static class AnimationTimerImpl extends AnimationTimer {
+        private static final boolean SCALING = true;
+        private static final int NUM_FRAMES_PER_RUN = 100;
+        private static final int NUM_RUNS = 10;
+        private final StringBuilder log = new StringBuilder();
+        private final long[][] frameTimes = new long[NUM_RUNS][NUM_FRAMES_PER_RUN];
+        private final Rectangle2D bounds;
+        private final Vector<SizedFXApp> pendingApps;
+        private final Canvas canvas;
+        private SizedFXApp runningApp;
+        private int frameIndex = 0;
+        private int runIndex = 0;
+
+        AnimationTimerImpl(Canvas canvas, Rectangle2D bounds, Vector<SizedFXApp> apps) {
+            this.canvas = canvas;
+            this.bounds = bounds;
+            pendingApps = apps;
+            restartWithNextApp();
+        }
+
+        @Override
+        public void handle(long aCurrentNanoTime) {
+            frameTimes[runIndex][frameIndex] = aCurrentNanoTime;
+            drawFrame();
+            frameIndex++;
+
+            if (frameIndex == NUM_FRAMES_PER_RUN) {
+                runIndex++;
+                frameIndex = 0;
+                runningApp.setSize(runIndex);
+                runningApp.settings();
+                runningApp.setup();
+            }
+
+            if (runIndex == NUM_RUNS) {
+                writeResults();
+
+                if (pendingApps.isEmpty()) {
+                    System.exit(0);
                 } else {
-                    double xoffset = (bounds.getWidth() - app.width) / 2;
-                    double yoffset = (bounds.getHeight() - app.height) / 2;
-                    canvas.getGraphicsContext2D().translate(xoffset, yoffset);
-                }
-
-                app.draw();
-                canvas.getGraphicsContext2D().restore();
-
-                frameIndex++;
-
-                if (frameIndex == NUM_FRAMES_PER_RUN) {
-                    runIndex++;
-                    frameIndex = 0;
-                    app.setSize(runIndex);
-                    app.settings();
-                    app.setup();
-                }
-
-                if (runIndex == NUM_RUNS) {
-                    System.out.println("Terminating");
-
-                    double[] runAvgFrameMs = new double[NUM_RUNS];
-                    double[] runSize = new double[NUM_RUNS];
-                    for (int i = 0; i < NUM_RUNS; i++) {
-                        long[] timesForRun = frameTimes[i];
-                        long totalRunTimeNs = timesForRun[timesForRun.length - 1] - timesForRun[0];
-                        double avgFrameGapMs = totalRunTimeNs / (NUM_FRAMES_PER_RUN * 1e6);
-                        log.append(avgFrameGapMs);
-                        log.append("\n");
-
-                        runSize[i] = i+1;
-                        runAvgFrameMs[i] = avgFrameGapMs;
-                        runIndex++;
-                    }
-
-                    try {
-                        FileWriter fw = new FileWriter("logs/" + app.name() + ".log");
-                        fw.write(log.toString());
-                        fw.close();
-                        System.out.println(Arrays.toString(runAvgFrameMs));
-
-                        //Chart Code
-
-                        XYChart chart = new XYChartBuilder().width(800).height(600).title("Frame Calculation Time (BB)")
-                                .xAxisTitle("size").yAxisTitle("ms").build();
-                        chart.getStyler().setYAxisMin(0d);
-                        chart.getStyler().setDefaultSeriesRenderStyle(XYSeries.XYSeriesRenderStyle.Scatter);
-                        chart.getStyler().setLegendVisible(false);
-                        chart.getStyler().setMarkerSize(8);
-                        chart.addSeries("ms per Frame", runSize, runAvgFrameMs);
-                        //Chart Code
-
-                        BitmapEncoder.saveBitmap(chart, "./logs/FCT_Bouncing_Balls", BitmapEncoder.BitmapFormat.PNG);
-                    } catch (IOException e) {
-                        e.printStackTrace();
-                    }
-
-                    if (apps.size() == 0) {
-                        System.exit(0);
-                    } else {
-                        app = apps.remove(0);
-                        System.out.println(app.name());
-                        System.out.println(app.description());
-                        frameIndex = 0;
-                        runIndex = 0;
-                        app.setSize(0);
-                    }
+                    restartWithNextApp();
                 }
             }
-        }.start();
+        }
 
-        stage.show();
+        private void restartWithNextApp() {
+            runningApp = pendingApps.remove(0);
 
+            String appDetails = String.format("%s\n%s\n", runningApp.name(), runningApp.description());
+            System.out.print(appDetails);
+            log.append(appDetails);
+
+            frameIndex = 0;
+            runIndex = 0;
+            runningApp.setSize(0);
+
+        }
+
+        private RunStats runStats() {
+            RunStats runStats = new RunStats(
+                    new double[NUM_RUNS],
+                    new double[NUM_RUNS]);
+
+            for (int i = 0; i < NUM_RUNS; i++) {
+                long[] timesForRun = frameTimes[i];
+                long totalRunTimeNs = timesForRun[timesForRun.length - 1] - timesForRun[0];
+                runStats.averageFrameMs[i] = totalRunTimeNs / (NUM_FRAMES_PER_RUN * 1e6);
+                runStats.size[i] = i + 1;
+            }
+            return runStats;
+        }
+
+        private void writeResults() {
+            RunStats runStats = runStats();
+
+            for (double avgFrameMs : runStats.averageFrameMs) {
+                log.append(avgFrameMs);
+                log.append("\n");
+            }
+
+            try {
+                writeRessultsFile(runStats);
+                writeChart(runStats);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+        private void writeRessultsFile(RunStats runStats) throws
+                IOException {
+            FileWriter fw = new FileWriter("logs/" + runningApp.name() + ".log");
+            fw.write(log.toString());
+            fw.close();
+            System.out.println(Arrays.toString(runStats.averageFrameMs));
+        }
+
+        private void writeChart(RunStats runStats) throws
+                IOException {
+            XYChart chart = new XYChartBuilder()
+                    .width(800)
+                    .height(600)
+                    .title("Frame Calculation Time (BB)")
+                    .xAxisTitle("size")
+                    .yAxisTitle("ms")
+                    .build();
+
+            chart.getStyler().setYAxisMin(0d);
+            chart.getStyler().setDefaultSeriesRenderStyle(XYSeries.XYSeriesRenderStyle.Scatter);
+            chart.getStyler().setLegendVisible(false);
+            chart.getStyler().setMarkerSize(8);
+            chart.addSeries("ms per Frame", runStats.size, runStats.averageFrameMs);
+
+            BitmapEncoder.saveBitmap(chart, "./logs/FCT_Bouncing_Balls", BitmapEncoder.BitmapFormat.PNG);
+        }
+
+        private void drawFrame() {
+            canvas.getGraphicsContext2D().save();
+            if (SCALING) {
+                canvas.getGraphicsContext2D()
+                        .scale(bounds.getWidth() / runningApp.width, bounds.getHeight() / runningApp.height);
+            } else {
+                double xOffset = (bounds.getWidth() - runningApp.width) / 2;
+                double yOffset = (bounds.getHeight() - runningApp.height) / 2;
+                canvas.getGraphicsContext2D().translate(xOffset, yOffset);
+            }
+
+            runningApp.draw();
+            canvas.getGraphicsContext2D().restore();
+        }
     }
 
     public static void main(String[] args) {
