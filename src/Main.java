794a6d56ba9150f3dd3cde218f995635ae0760a8
georgiecurtis <georgina.curtis@students.mq.edu.au>
2019-12-18 15:06:23 +1100

    change startup from 'yeet' to the 'ApplicationChooser'

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ import processing.core.*;
 public class Main {
   static public void main(String[] passedArgs) {
     String[] appletArgs = new String[] { "--full-screen", "--bgcolor=#666666", "--stop-color=#cccccc", ""};
-    PApplet.runSketch(appletArgs, new studentwork.Yeet());
+    PApplet.runSketch(appletArgs, new mqapp.ApplicationChooser());
   }
 
 }
