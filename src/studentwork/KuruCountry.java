d1f63ad8180da0d4f74c6ac4014aade95bec79b4
Beth <elizabeth.cappellazzo@students.mq.edu.au>
2019-12-20 13:00:13 +1100

    Added author name for Kuru Country

--- src/studentwork/KuruCountry.java
+++ src/studentwork/KuruCountry.java
@@ -4,7 +4,8 @@ import javafx.scene.canvas.GraphicsContext;
 
 public class KuruCountry extends mqapp.MQApp { //important
 
-    public String name(){return "Kuru Country - Chris Felix";}
+    public String name(){return "Kuru Country";}
+    public String author(){return "Chris Felix";}
     public String description(){return "";}
 
     //public KuruCountry(GraphicsContext g){super(g);} - found to be unnecessary so far
